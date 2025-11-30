61b09018512476f9be5ce0195b86486ada0152bf
Aidan Malana <aidanmalana02@gmail.com>
2025-11-30 13:24:50 -0600

    verlet works!

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ window_size = (1280, 720)
 screen = pygame.display.set_mode(window_size)
 
 running = True
+dt = 0
 clock = pygame.time.Clock()
 
-GRAVITY = 9.8
+GRAVITY = Vector2(0, 10)
+
+container = {"position": Vector2(screen.width / 2, screen.height / 2), "radius": 360}
 
 class Ball:
     def __init__(self, p_current: Vector2, vel: Vector2, mass, radius):
@@ -26,12 +29,19 @@ class Ball:
     def apply_force(self, sum_of_forces: Vector2):
         self.a = sum_of_forces/self.mass
 
-    def update_position(self):
-        self.p_current += (0, 10)
-        # update velocity
+    def update_position(self, dt):
+        tmp = self.p_current
+        self.p_current = self.p_current * 2 - self.p_old + self.a * (dt ** 2)
+        self.p_old = tmp
+        if ball.p_current.distance_to(container["position"]) > container["radius"]:
+            delta = Vector2((ball.p_current - container["position"])[0], (ball.p_current - container["position"])[1])
+            delta.scale_to_length(container["radius"])
+            ball.p_current = delta + container["position"]
+            ball.p_old = ball.p_current + ball.vel
+        self.vel = self.p_current - self.p_old
 
 balls: List[Ball] = []
-balls.append(Ball(Vector2(screen.width/2, 0), Vector2(0, 0), 1, 10))
+balls.append(Ball(Vector2(screen.width/2, 20), Vector2(0, 0), 1, 10))
 
 while running:
     for event in pygame.event.get():
@@ -39,14 +49,15 @@ while running:
             running = False
 
     for ball in balls:
-        ball.updatePosition()
+        ball.apply_force(GRAVITY)
+        ball.update_position(dt)
 
     screen.fill("black")
 
-    # pygame.draw.circle(screen, "white", (balls[0][0], balls[0][1]), 10)
+    pygame.draw.circle(screen, "white", container["position"], container["radius"], 2)
     for ball in balls:
         pygame.draw.circle(screen, "white", ball.p_current, ball.radius)
     
     pygame.display.flip()
-    clock.tick(60)
+    dt = clock.tick(60) / 1000
 pygame.quit()
